d50dbccb74542543001ac9b1aa42680ca8d49c47
kim pahud <Kphd64@gmail.com>
2024-01-15 16:05:13 -0500

    V final Plots tourelles

--- Assets/BuildArcher.cs
+++ Assets/BuildArcher.cs
@@ -3,16 +3,24 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BuildArcher : MonoBehaviour
 {
+    public GameObject LastButtonClick;
+    public GameObject PanelChoixTours;
+    public GameObject prefabArcher;
     public Vector2 TourCoo;
-    public void Start()
+    public void onClickBaby()
     {
         GameObject gameObjectSource = GameObject.Find("Plots");
         GestionBoutton gestionboutton = gameObjectSource.GetComponent<GestionBoutton>();
         TourCoo = gestionboutton.PosLastClickButton;
+        LastButtonClick = (GameObject)gestionboutton.LastClickButton.gameObject;
+        //LastButtonClick = gestionboutton.LastClickButton.gameObject as GameObject;
+        GameObject InstanceSpawn = Instantiate(prefabArcher, TourCoo, Quaternion.identity);
+        PanelChoixTours.SetActive(false);
+        LastButtonClick.SetActive(false);
+
     }
-    [Header("References")]
-    [SerializeField] public GameObject[] towerprefabs;
 }
