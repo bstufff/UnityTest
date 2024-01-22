71c0a1147db1d46c185585655bd29ef942e453f8
bstuff <arthur.bohn@lcieducation.net>
2024-01-22 11:41:19 -0500

    KIM ARRETE DE CHOMER

--- Assets/Script/EnemyMovement.cs
+++ Assets/Script/EnemyMovement.cs
@@ -5,10 +5,10 @@ using UnityEngine;
 public class EnemyMovement : MonoBehaviour
 {
     [Header("References")]
-    [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Rigidbody2D rb;//Rigidbody de l'ennemi : pour l'assigner il faut dragndrop sous unity le rigidbody de l'ennemi dans le champ corre
 
     [Header("Attributes")]
-    [SerializeField] private float moveSpeed = 2f;
+    [SerializeField] private float moveSpeed = 2f;//Valeur par défaut de la vitesse
 
     private Transform target;//Point we want to move to
     private int pathIndex = 0;//Index du chemin 
@@ -19,23 +19,24 @@ public class EnemyMovement : MonoBehaviour
     }
     private void Update()
     {
-        if (Vector2.Distance(target.position, transform.position) <= 0.1f) {
+        if (Vector2.Distance(target.position, transform.position) <= 0.1f) {//Si la distance entre l'ennemi et le point est de moins de 0.1, il passe au point suivant
             pathIndex++;//Passe au prochain point
            
-            if (pathIndex == LevelManager.main.path.Length) { 
-                Destroy(gameObject);
+            if (pathIndex == LevelManager.main.path.Length) { //Si l'index des points du chemin est la même que la longueur de la liste "path", détruire l'ennemi
+                Destroy(gameObject);//no shit
+                //foutre le code pour enlever les vies
                 return;
             }
             else
             {
-                target = LevelManager.main.path[pathIndex];
+                target = LevelManager.main.path[pathIndex];//Assigne la position du point au prochain élément dans la liste
             }
         }
     }
     private void FixedUpdate()
     {
-        Vector2 direction = (target.position - transform.position).normalized;
+        Vector2 direction = (target.position - transform.position).normalized;//Regarde le point
 
-        rb.velocity = direction * moveSpeed;
+        rb.velocity = direction * moveSpeed;//Se déplace vers le point
     }
 }
--- Assets/Script/EnemySpawner.cs
+++ Assets/Script/EnemySpawner.cs
@@ -7,38 +7,32 @@ public class EnemySpawner : MonoBehaviour
 {
 
     [Header("References")]
-    [SerializeField] private GameObject[] enemyPrefabs;
-
+    [SerializeField] private GameObject[] enemyPrefabs;//Array contenant les prefabs des ennemis 
     [Header("Attributes")]
 
-    //[SerializeField] private float timeBetweenWaves = 5f;
-
-    private int waveIndex = 0;
-    //private bool isSpawing = false;
-
-    private List<Wave> waveList = new List<Wave>();
-    private Wave waveToSpawn;
+    [SerializeField]private List<Wave> waveList = new List<Wave>();//Liste contenant toutes les waves du jeu
     [System.Serializable]
     public class EnemyGroup
     {
-        public int pathTaken;
-        public int amountOfEnemies;
-        public int enemyType;
-        public EnemyGroup(int _pathTaken, int _amountOfEnemies, int _enemyType)
+        public int pathTaken;//Chemin pris par les ennemis
+        public int amountOfEnemies;//Quantité d'ennemis dans le groupe
+        public int enemyType;//Type d'ennemi : entier correspondant à la position du prefab séléctionné dans enemyPrefabs
+        public EnemyGroup(int _pathTaken, int _amountOfEnemies, int _enemyType)//Fonction pour initialisation
         {
             pathTaken = _pathTaken;
             amountOfEnemies = _amountOfEnemies;
             enemyType = _enemyType;
         }
     }
+    [System.Serializable]
     public class EnemyFormation
     {
 
-        public List<EnemyGroup> Formation;
-        public int numberOfGroups;
+        public List<EnemyGroup> Formation;//Liste des groupes d'ennemis présents dans la formation
+        public int numberOfGroups;//no shit
         public int spacing;//spacing de 1 = 0.1s entre les spawn
-        public int delay = 0;
-        public EnemyFormation(List<EnemyGroup> _formation, int _numberOfGroups, int _spacing, int _delay)
+        public int delay = 0;//délai avant le lancement de cette formation, si = 0 la formation commencera à spawn juste après la fin de la précédente
+        public EnemyFormation(List<EnemyGroup> _formation, int _numberOfGroups, int _spacing, int _delay)//Fonction pour initialisation
         {
             numberOfGroups = _numberOfGroups;
             Formation = _formation;
@@ -46,11 +40,12 @@ public class EnemySpawner : MonoBehaviour
             delay = _delay;
         }
     }
+    [System.Serializable]
     public class Wave
     {
-        public int amountOfFormations;
-        public List<EnemyFormation> waveContents = new List<EnemyFormation>();
-        public Wave(int _amountOfFormations, List<EnemyFormation> _waveContents)
+        public int amountOfFormations;//no shit
+        public List<EnemyFormation> waveContents = new List<EnemyFormation>();//Liste des formations d'ennemis contenues dans la vague
+        public Wave(int _amountOfFormations, List<EnemyFormation> _waveContents)//Fonction pour initialisation
         {
             amountOfFormations = _amountOfFormations;
             waveContents = _waveContents;
@@ -63,28 +58,36 @@ public class EnemySpawner : MonoBehaviour
 
     private void Start()
     {
-        Wave wave1 = new Wave(1,new List<EnemyFormation>{ new EnemyFormation(new List<EnemyGroup> {new EnemyGroup(0,10,0)},1, 10, 0) });
+        /*Exemple de wave
+        Wave wave1 = new Wave(1,new List<EnemyFormation>{ 
+            new EnemyFormation(new List<EnemyGroup> {
+                new EnemyGroup(0,10,0)
+            },1, 10, 0) 
         
-        waveList.Add(wave1);
-        StartCoroutine(StartWave());
+        });
+        waveList.Add(wave1);//Ajout à wavelist */
+        foreach (Wave CurrentWave in waveList) {
+            StartCoroutine(StartWave(CurrentWave));//Commence une coroutine qui lance la vague (faire une coroutine permet d'ajouter du délai sans déranger le reste du programme)
+        }
     }
 
-    IEnumerator StartWave()
+    IEnumerator StartWave(Wave waveToSpawn)
     {
-        waveToSpawn = waveList[waveIndex];
-
-        for (int i = 0; i < waveToSpawn.amountOfFormations; i++)
+        for (int i = 0; i < waveToSpawn.amountOfFormations; i++)//Boucle des formations
         {
-            EnemyFormation currentFormation = waveToSpawn.waveContents[i];
-            for (int j = 0; j < currentFormation.numberOfGroups; j++)
+            EnemyFormation currentFormation = waveToSpawn.waveContents[i];//Séléctionne la formation
+            yield return new WaitForSeconds(currentFormation.delay);//Délai avant que la formation apparaisse
+            for (int j = 0; j < currentFormation.numberOfGroups; j++)//Boucle des groupes
             {
-                EnemyGroup currentGroup = currentFormation.Formation[j];
-                for (int k = 0; k < currentGroup.amountOfEnemies; k++)
+                EnemyGroup currentGroup = currentFormation.Formation[j];//Séléctionne le groupe d'ennemis
+                for (int k = 0; k < currentGroup.amountOfEnemies; k++)//Boucle des ennemis
                 {
-                    Instantiate(enemyPrefabs[currentGroup.enemyType], LevelManager.main.startPoint.position, Quaternion.identity);
-                    yield return new WaitForSeconds(4);
+                    Instantiate(enemyPrefabs[currentGroup.enemyType], LevelManager.main.startPoint.position, Quaternion.identity);//Fait apparaitre un ennemi
+                    yield return new WaitForSeconds(4);//Délai entre les spawns
                 }
             }
         }
+        yield return new WaitForSeconds(15);//Délai de 15 secondes après la fin d'une vague
+        //Maybe un moyen visuel d'indiquer ce délai entre les vagues ?
     }
 }
