103ca5b346e0b07e5a7ba10bbc1e126ebe6048c4
kim pahud <Kphd64@gmail.com>
2024-01-31 14:39:15 -0500

    ajout boutton spawn Slowing + bugfix

---
+++ Assets/Script/Enemy/Script Communs/Variables.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    //Variables générales
+    public int Pv = 10;
+    public int DegatsMob = 10;
+    public int DegatsChateau = 10;
+    public float Speed = 3f;
+    public int ValeurGold = 15;
+    
+}
