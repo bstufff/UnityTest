c4400d544898d7e149d0a9ccacc1e2d011978422
Jean-Cabinet <nathan.isarte@lcieducation.net>
2024-02-02 14:10:43 -0500

    Auto stash before merge of "main" and "origin/main"

---
+++ Assets/Mouvementallies.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mouvementallies : MonoBehaviour
+{
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(1)) 
+        { 
+
+
+
+        
+        }
+
+        
+    }
+
+
+
+
+
+
+
+
+}
