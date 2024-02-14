6c500383b78d9b7d4e3e15bb2fba82fbee9a5692
bstuff <arthur.bohn@lcieducation.net>
2024-02-14 15:14:22 -0500

    chevaliers ont des noms
    micro commit, les chevaliers qui spawn auront automatiquement 1,2,3 comme noms pour mieux les différencier

--- Assets/Script/Ally/Script Blocus/BlocusSpawn.cs
+++ Assets/Script/Ally/Script Blocus/BlocusSpawn.cs
@@ -16,8 +16,11 @@ public class BlocusSpawn : MonoBehaviour
         GameObject InstanciateGameobject2 = Instantiate(Chevalier, Posblocus.position, Quaternion.identity);
         GameObject InstanciateGameobject3 = Instantiate(Chevalier, Posblocus.position, Quaternion.identity);
         InstanciateGameobject1.transform.parent = ParentGameObject;
+        InstanciateGameobject1.name = "1";
         InstanciateGameobject2.transform.parent = ParentGameObject;
-        InstanciateGameobject3.transform.parent = ParentGameObject; 
+        InstanciateGameobject2.name = "2";
+        InstanciateGameobject3.transform.parent = ParentGameObject;
+        InstanciateGameobject3.name = "3";
         //additionner vecteur si on veut un décalage par rapport à l'origine
     }
 
